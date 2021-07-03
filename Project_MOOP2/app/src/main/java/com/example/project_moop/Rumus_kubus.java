4ec206016d9ee614c25d11fb7b67b551c4cd7841
blackver69 <46539728+blackver69@users.noreply.github.com>
2021-07-03 21:22:17 +0700

    Update Rumus_kubus.java

--- Project_MOOP2/app/src/main/java/com/example/project_moop/Rumus_kubus.java
+++ Project_MOOP2/app/src/main/java/com/example/project_moop/Rumus_kubus.java
@@ -1,6 +1,6 @@
 package com.example.project_moop;
 
-public class Rumus_kubus extends Rumus_Persegi implements BangunRuang {
+public class Rumus_kubus extends Rumus_Persegi implements BangunRuang, Soal_Quiz {
 
 
     public Rumus_kubus(float sisi) {
@@ -18,5 +18,10 @@ public class Rumus_kubus extends Rumus_Persegi implements BangunRuang {
     public float LuasPermukaan() {
         float LP= hitungLuas()* 6;
         return LP;
+    }
+    @Override
+    public void random_soal() {
+        Random ran= new Random();
+        sisi= (float) (ran.nextInt(30));
     }
 }
